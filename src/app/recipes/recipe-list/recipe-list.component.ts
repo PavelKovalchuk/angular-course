0d9917af28a85c0d8b055b21d19803a22f2aa893
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-16 12:53:43 +0300

    add recipe detail markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
+
+  recipes: Recipe[] = [
+    new Recipe("TestName 2", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
+    new Recipe("TestName", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
+  ];
 
   ngOnInit() {
   }
