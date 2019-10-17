a7a8f9a566648005fbca5511649625155995adf6
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-17 15:23:18 +0300

    service as cross component comunication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,17 +9,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    console.log("this.recipeService ", this.recipeService);
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(element: Recipe) {
-    this.recipeWasSelected.emit(element);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 /* @Injectable({
   providedIn: 'root'
 }) */
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe("TestName 2", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
     new Recipe("TestName", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
