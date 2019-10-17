c9687da3a1a17bd95897b0701398224dc3a86c43
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-17 15:36:28 +0300

    service with event emiter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,23 @@
-import { Ingredient } from './../shared/ingredient.model';
-import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from "./shopping-list.service";
+import { Ingredient } from "./../shared/ingredient.model";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  constructor(private SLService: ShoppingListService) {}
 
-  constructor() { }
-
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredients: Ingredient[] = [];
 
   ngOnInit() {
+    this.ingredients = this.SLService.getIngredients();
+    this.SLService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
-
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
-
 }
