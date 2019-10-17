c312cab65785560622fa01f53e9268ad1b2c2b15
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-17 15:44:58 +0300

    add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { Injectable, EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from '../shared/ingredient.model';
 
 /* @Injectable({
   providedIn: 'root'
@@ -7,13 +8,26 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe("TestName 2", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
-    new Recipe("TestName", "TestDescription", "https://hips.hearstapps.com/delish/assets/17/37/1505333248-goulash-delish-1.jpg"),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
     return this.recipes.slice();
   }
 
-  constructor() { }
+  constructor() {}
 }
