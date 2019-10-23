c6bfc202b37a6b4d419ddaca68f50104a71c532c
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-23 11:14:17 +0300

    add lazy loading basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,18 @@ const appRoutes: Routes = [
     redirectTo: "/recipes",
     pathMatch: "full"
   },
+  {
+    path: "recipes",
+    loadChildren: "./recipes/recipes.module#RecipesModule"
+  },
+  {
+    path: "shopping-list",
+    loadChildren: "./shopping-list/shopping-list.module#ShoppingListModule"
+  },
+  {
+    path: "auth",
+    loadChildren: "./auth/auth.module#AuthModule"
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from "./app-routing.module";
 import { HeaderComponent } from "./header/header.component";
 import { BrowserModule } from "@angular/platform-browser";
@@ -7,8 +6,6 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 
@@ -22,11 +19,11 @@ import { CoreModule } from './core.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
+    // RecipesModule, // Lazy loaded module
+   // ShoppingListModule,  // Lazy loaded module
     SharedModule,
     CoreModule,
-    AuthModule
+   // AuthModule // Lazy loaded module
   ],
   bootstrap: [AppComponent],
 })
